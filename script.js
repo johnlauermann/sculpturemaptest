21aaeac59524fba97290bb96873265bd057cc457
johnlauermann <72218401+johnlauermann@users.noreply.github.com>
2024-02-22 21:31:18 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,63 +3,71 @@ var map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v11',
     center: [-73.963, 40.691],
-    zoom: 14
+    zoom: 12
 });
 
-// Add a marker for each point from Google Sheets data
-// You can fetch and parse the Google Sheets data using Fetch API or other methods
+// Fetch Google Sheets data using Fetch API or other methods
+fetch('YOUR_GOOGLE_SHEETS_URL')
+    .then(response => response.json())
+    .then(data => {
+        // Process the data and add markers to the map
+        data.forEach(point => {
+            var marker = new mapboxgl.Marker()
+                .setLngLat([point.longitude, point.latitude])
+                .addTo(map)
+                .setPopup(new mapboxgl.Popup().setHTML('<h3>' + point.title + '</h3><p>' + point.description + '</p>'))
+                .addTo(map)
+                .getElement()
+                .addEventListener('click', function() {
+                    var sidebarContent = '<h2>' + point.title + '</h2>';
+                    sidebarContent += '<p>' + point.description + '</p>';
 
-// Add click event listener to display point attributes and galleries in the sidebar
-map.on('click', 'points', function (e) {
-    var pointAttributes = e.features[0].properties;
+                    sidebarContent += '<div id="imageGallery">';
+                    point.images.forEach(function(imageUrl) {
+                        sidebarContent += '<img src="' + imageUrl + '" />';
+                    });
+                    sidebarContent += '</div>';
 
-    var sidebarContent = '<h2>' + pointAttributes.title + '</h2>';
-    sidebarContent += '<p>' + pointAttributes.description + '</p>';
+                    sidebarContent += '<div id="pdfGallery">';
+                    point.pdfs.forEach(function(pdfUrl) {
+                        sidebarContent += '<embed src="' + pdfUrl + '" type="application/pdf" width="100%" height="600px" />';
+                    });
+                    sidebarContent += '</div>';
 
-    sidebarContent += '<div id="imageGallery">';
-    pointAttributes.images.forEach(function(imageUrl) {
-        sidebarContent += '<img src="' + imageUrl + '" />';
-    });
-    sidebarContent += '</div>';
+                    document.getElementById('sidebar').innerHTML = sidebarContent;
 
-    sidebarContent += '<div id="pdfGallery">';
-    pointAttributes.pdfs.forEach(function(pdfUrl) {
-        sidebarContent += '<embed src="' + pdfUrl + '" type="application/pdf" width="100%" height="600px" />';
-    });
-    sidebarContent += '</div>';
+                    // Initialize Fancybox for image gallery
+                    $("#imageGallery").find('img').each(function() {
+                        $(this).wrap('<a data-fancybox="gallery" href="' + $(this).attr('src') + '"></a>');
+                    });
 
-    document.getElementById('sidebar').innerHTML = sidebarContent;
+                    // Display PDF files using PDF.js
+                    document.getElementById('pdfGallery').querySelectorAll('embed').forEach(function(embedElement) {
+                        var pdfUrl = embedElement.getAttribute('src');
 
-    // Initialize Fancybox for image gallery
-    $("#imageGallery").find('img').each(function() {
-        $(this).wrap('<a data-fancybox="gallery" href="' + $(this).attr('src') + '"></a>');
-    });
+                        pdfjsLib.getDocument(pdfUrl).promise.then(function(pdfDoc) {
+                            pdfDoc.getPage(1).then(function(page) {
+                                var scale = 1.5;
+                                var viewport = page.getViewport({ scale: scale });
 
-    // Display PDF files using PDF.js
-    document.getElementById('pdfGallery').querySelectorAll('embed').forEach(function(embedElement) {
-        var pdfUrl = embedElement.getAttribute('src');
+                                var canvas = document.createElement('canvas');
+                                var context = canvas.getContext('2d');
+                                canvas.height = viewport.height;
+                                canvas.width = viewport.width;
 
-        pdfjsLib.getDocument(pdfUrl).promise.then(function(pdfDoc) {
-            pdfDoc.getPage(1).then(function(page) {
-                var scale = 1.5;
-                var viewport = page.getViewport({ scale: scale });
+                                var renderContext = {
+                                    canvasContext: context,
+                                    viewport: viewport
+                                };
 
-                var canvas = document.createElement('canvas');
-                var context = canvas.getContext('2d');
-                canvas.height = viewport.height;
-                canvas.width = viewport.width;
-
-                var renderContext = {
-                    canvasContext: context,
-                    viewport: viewport
-                };
-
-                page.render(renderContext);
-                embedElement.parentNode.replaceChild(canvas, embedElement);
-            });
+                                page.render(renderContext);
+                                embedElement.parentNode.replaceChild(canvas, embedElement);
+                            });
+                        });
+                    });
+                });
         });
     });
-});
 
 // Add layers and sources to the map for displaying points
 
