3a46e51aa8035bd035b17cd334a9a6bd29f3222d
johnlauermann <72218401+johnlauermann@users.noreply.github.com>
2024-02-22 21:36:19 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,75 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiamxhdWVybWEiLCJhIjoiY2xzeTByanM2MDh2NzJrbzJ1czk1NDhxMSJ9.jc_JLbR6Hn2OWQFC9b6ClA';
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: [-73.963, 40.691],
+    zoom: 12
+});
 
+// Fetch Google Sheets data using Fetch API or other methods
+fetch('YOUR_GOOGLE_SHEETS_URL')
+    .then(response => response.json())
+    .then(data => {
+        // Process the data and add markers to the map
+        data.forEach(point => {
+            var marker = new mapboxgl.Marker()
+                .setLngLat([point.longitude, point.latitude])
+                .addTo(map)
+                .setPopup(new mapboxgl.Popup().setHTML('<h3>' + point.title + '</h3><p>' + point.description + '</p>'))
+                .addTo(map)
+                .getElement()
+                .addEventListener('click', function() {
+                    var sidebarContent = '<h2>' + point.title + '</h2>';
+                    sidebarContent += '<p>' + point.description + '</p>';
+
+                    sidebarContent += '<div id="imageGallery">';
+                    point.images.forEach(function(imageUrl) {
+                        sidebarContent += '<img src="' + imageUrl + '" />';
+                    });
+                    sidebarContent += '</div>';
+
+                    sidebarContent += '<div id="pdfGallery">';
+                    point.pdfs.forEach(function(pdfUrl) {
+                        sidebarContent += '<embed src="' + pdfUrl + '" type="application/pdf" width="100%" height="600px" />';
+                    });
+                    sidebarContent += '</div>';
+
+                    document.getElementById('sidebar').innerHTML = sidebarContent;
+
+                    // Initialize Fancybox for image gallery
+                    $("#imageGallery").find('img').each(function() {
+                        $(this).wrap('<a data-fancybox="gallery" href="' + $(this).attr('src') + '"></a>');
+                    });
+
+                    // Display PDF files using PDF.js
+                    document.getElementById('pdfGallery').querySelectorAll('embed').forEach(function(embedElement) {
+                        var pdfUrl = embedElement.getAttribute('src');
+
+                        pdfjsLib.getDocument(pdfUrl).promise.then(function(pdfDoc) {
+                            pdfDoc.getPage(1).then(function(page) {
+                                var scale = 1.5;
+                                var viewport = page.getViewport({ scale: scale });
+
+                                var canvas = document.createElement('canvas');
+                                var context = canvas.getContext('2d');
+                                canvas.height = viewport.height;
+                                canvas.width = viewport.width;
+
+                                var renderContext = {
+                                    canvasContext: context,
+                                    viewport: viewport
+                                };
+
+                                page.render(renderContext);
+                                embedElement.parentNode.replaceChild(canvas, embedElement);
+                            });
+                        });
+                    });
+                });
+        });
+    });
+
+// Add layers and sources to the map for displaying points
+
+// Add navigation control to the map
+map.addControl(new mapboxgl.NavigationControl());
